fce0d9e25011dc4dfca5d374f37a0365534883a7
Alexwell <935562189@qq.com>
2023-02-16 20:49:01 +0100

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,6 +1,8 @@
 import time
 import torch
 from torch.profiler import profile, record_function, ProfilerActivity
+from tqdm import tqdm
+import evaluate
 
 class ProfilingTrainer:
     def __init__(
@@ -24,6 +26,8 @@ class ProfilingTrainer:
         self.model.to(self.device)
         self.optimizer.zero_grad()
         train_start_time = time.time()
+
+        progress_bar = tqdm(range(n_epochs * len(self.train_dataloader)), desc="Epoch")
         
         for epoch in range(n_epochs):
             epoch_start_time = time.time()
@@ -33,9 +37,10 @@ class ProfilingTrainer:
 
                 with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True) as prof:
                     with record_function("forward"):
+                        self.model.zero_grad()
                         outputs = self.model(**batch)
                     with record_function("backward"):
-                        loss = outputs[0]
+                        loss = outputs.loss
                         loss.backward()
                         self.optimizer.step()
                         self.scheduler.step()
@@ -44,13 +49,16 @@ class ProfilingTrainer:
                 if (step + 1) % self.n_steps_per_val == 0:
                     val_acc = self.evaluate()
                     self.val_logs.append({'step': step, 'accuracy': val_acc})
+                    print(f"Validation accuracy at step {step+1}: {val_acc:.2f}, loss: {loss.item():.2f}")
                     if val_acc >= self.target_val_acc:
                         print(f"Stopping training at epoch {epoch+1}, step {step+1} as target validation accuracy reached")
                         self.train_time = time.time() - train_start_time
                         self.profiler_log = prof.key_averages().table(sort_by="cpu_time_total")
                         return
+                    
                 
                 self.training_logs.append({'epoch': epoch, 'step': step, 'loss': loss.item()})
+                progress_bar.update(1)
             epoch_end_time = time.time()
             epoch_time = epoch_end_time - epoch_start_time
             print(f"Epoch {epoch+1} took {epoch_time:.2f} seconds.")
@@ -60,15 +68,14 @@ class ProfilingTrainer:
     
     def evaluate(self):
         self.model.eval()
-        total_correct = 0
-        total = 0
+        metric = evaluate.load("accuracy")
         with torch.no_grad():
             for batch in self.val_dataloader:
-                input_ids = batch['input_ids'].to(self.device)
-                attention_mask = batch['attention_mask'].to(self.device)
-                labels = batch['labels'].to(self.device)
-                outputs = self.model(input_ids=input_ids, attention_mask=attention_mask)
-                preds = torch.argmax(outputs.logits, dim=1)
-                total_correct += torch.sum(preds == labels)
-                total += len(labels)
-        return total_correct / total
+                batch = {k: v.to(self.device) for k, v in batch.items()}
+                outputs = self.model(**batch)
+                logits = outputs.logits
+                predictions = torch.argmax(logits, dim=-1)
+                metric.add_batch(predictions=predictions, references=batch["labels"])
+        self.model.train()
+        return metric.compute()['accuracy']
+        
