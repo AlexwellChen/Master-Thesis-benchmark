f4de2ab54d34d67da37eed8dbe25351b7336c4c1
Alexwell <935562189@qq.com>
2023-02-20 14:20:21 +0100

    Update glue_mrpc_bert_based.py

--- glue_mrpc_bert_based.py
+++ glue_mrpc_bert_based.py
@@ -91,7 +91,7 @@ if __name__ == '__main__':
     # Add the argument for number of n_steps_per_val
     parser.add_argument('--n_steps_per_val', type=int, default=50)
     # Add the argument for target_val_acc
-    parser.add_argument('--target_val_acc', type=float, default=None)
+    parser.add_argument('--target_val_acc', type=float, default=0.84)
     # Add the name for log file
     parser.add_argument('--log_file_name', type=str, default='profiling')
     # Wether to use fused optimizer
