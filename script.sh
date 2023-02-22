8725457609f38fef652a0b0db46bb170dfd12006
Alexwell <935562189@qq.com>
2023-02-22 08:30:11 +0100

    Change script

--- script.sh
+++ script.sh
@@ -3,19 +3,19 @@ python glue_mrpc_bert_based.py \
         --n_epochs 10 --warmup 160 \
         --lr 1e-5 --wd 0.01 \
         --optimizer adamw \
-        --log_file_name adamw_lr1e-5_wd1e-2_160_10
+        --log_file_name adamw_lr1e-5_wd1e-2_wm160_ep10
 echo "--------------------------adamw done--------------------------"
 python glue_mrpc_bert_based.py \
         --n_epochs 10 --warmup 160 \
         --lr 1e-5 --wd 0.01 \
         --optimizer adam \
-        --log_file_name adam_lr1e-5_wd1e-2_160_10
+        --log_file_name adam_lr1e-5_wd1e-2_wm160_ep10
 echo "--------------------------adam done--------------------------"
 python glue_mrpc_bert_based.py \
-        --n_epochs 10 --warmup 320 \
-        --lr 5e-5 --wd 0.005 \
+        --n_epochs 10 --warmup 50 \
+        --lr 1e-4 --wd 0.01 \
         --optimizer adan \
-        --log_file_name adan_lr5e-5_wd5e-3_320_10
+        --log_file_name adan_lr1e-4_wd1e-2_wm50_ep10
 echo "--------------------------adan done--------------------------"
 
 # adamw:
