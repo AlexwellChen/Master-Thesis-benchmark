b179423b52aa97d3efa6a0dbc826c2d2dbdfaa62
Alexwell <935562189@qq.com>
2023-02-24 09:40:45 +0100

    Update

---
+++ README.md
@@ -0,0 +1,3 @@
+### Running scripts
+Please ensure you are at the root directory of the project before running any of the scripts.
+`user@host:~/path/to/project$sh scripts/SCRIPT_NAME.sh`
--- benchmark/imdb_bert_base.py
+++ benchmark/imdb_bert_base.py
@@ -57,13 +57,13 @@ def model_and_trainer(train_loader, test_loader, eval_loader, args):
     elif args.optimizer == 'adan':
         betas = (0.98, 0.92, 0.99)
         if args.fused_optimizer and args.foreach:
-            optimizer = Adan(model.parameters(), lr=args.lr, weight_decay=args, fused=True, foreach=True, betas=betas, eps=1e-8)
+            optimizer = Adan(params=model.parameters(), lr=args.lr, weight_decay=args, fused=True, foreach=True, betas=betas, eps=1e-8)
         elif args.fused_optimizer and not args.foreach:
-            optimizer = Adan(model.parameters(), lr=args.lr, weight_decay=args.wd, fused=True, foreach=False, betas=betas, eps=1e-8)
+            optimizer = Adan(params=model.parameters(), lr=args.lr, weight_decay=args.wd, fused=True, foreach=False, betas=betas, eps=1e-8)
         elif not args.fused_optimizer and args.foreach:
-            optimizer = Adan(model.parameters(), lr=args.lr, weight_decay=args.wd, fused=False, foreach=True, betas=betas, eps=1e-8)
+            optimizer = Adan(params=model.parameters(), lr=args.lr, weight_decay=args.wd, fused=False, foreach=True, betas=betas, eps=1e-8)
         else:
-            optimizer = Adan(model.parameters(), lr=args.lr, weight_decay=args.wd, fused=False, foreach=False, betas=betas, eps=1e-8)
+            optimizer = Adan(params=model.parameters(), lr=args.lr, weight_decay=args.wd, fused=False, foreach=False, betas=betas, eps=1e-8)
     # adamw
     elif args.optimizer == 'adamw':
         betas=(0.9, 0.999)
