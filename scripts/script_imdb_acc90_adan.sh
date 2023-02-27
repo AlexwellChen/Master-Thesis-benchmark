85ac9719aea8b666991451f9d254582cf044a572
Alexwell <935562189@qq.com>
2023-02-27 10:44:43 +0100

    Update
    Make adan use same data split

--- scripts/script_imdb_acc90_adan.sh
+++ scripts/script_imdb_acc90_adan.sh
@@ -1,20 +1,11 @@
 clear
 echo "-----------------------Benchmark start------------------------"
-python ./benchmark/imdb_bert_base.py \
+python ./benchmark/imdb_bert_base_adan.py \
         --n_epochs 2 --warmup 50 \
         --lr 1e-4 --wd 0.01 \
         --optimizer adan \
-        --log_file_name imdb_adan_lr1e-4_wd1e-2_wm50_ep2_acc90 \
         --target_val_acc 0.90
-echo "--------------------------adan done--------------------------"
-python ./benchmark/imdb_bert_base.py \
-        --n_epochs 2 --warmup 50 \
-        --lr 1e-4 --wd 0.01 \
-        --optimizer adan \
-        --log_file_name imdb_adan_fused_lr1e-4_wd1e-2_wm50_ep2_acc90 \
-        --target_val_acc 0.90 \
-        --fused_optimizer True
-echo "-----------------------fused adan done------------------------"
+echo "----------------------------- done----------------------------"
 # Plot the results
 python ./benchmark/plot_loss_accuracy.py IMDB_acc90
 # adamw:
