7cae17c09c1d3b8d6aec67eb3e37645986bc89fb
Alexwell <935562189@qq.com>
2023-03-04 09:53:53 +0100

    Update

--- scripts/script_imdb_acc90_mix_2gpu.sh
+++ scripts/script_imdb_acc90_mix_2gpu.sh
@@ -5,7 +5,7 @@ accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix_2gpu.
         --n_epochs 2 --warmup 50 \
         --lr 1e-4 --wd 0.01 \
         --optimizer adan \
-        --log_file_name imdb_adan_fused_lr1e-4_wd1e-2_wm50_ep2_acc90_mix \
+        --log_file_name imdb_adan_fused_lr1e-4_wd1e-2_wm50_ep2_acc90_mix_2gpu \
         --target_val_acc 0.90 \
         --fused_optimizer True
 echo "-----------------------2 GPU mix percision done------------------------"
