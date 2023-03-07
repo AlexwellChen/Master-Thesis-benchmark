8d41ec480baddde7a386d6177b502dbbc6c11ec8
Alexwell <935562189@qq.com>
2023-03-07 16:17:35 +0100

    Update adan_micro_benchmark.py

--- benchmark/adan_micro_benchmark.py
+++ benchmark/adan_micro_benchmark.py
@@ -2,11 +2,13 @@ import torch
 from adan import Adan
 import argparse
 
-def get_fake_parameters(n_params=10, size=512):
+def get_fake_parameters(n_params=10, size=512, fp16=False):
     params = []
     for i in range(n_params):
-        tensor = torch.randn(size, size, requires_grad=True, device='cuda')
+        tensor = torch.randn(size, size, requires_grad=True, device='cuda')   
         tensor.grad = torch.randn(size, size, device='cuda')
+        if fp16:
+            tensor.grad = tensor.grad.half() # Only grad is fp16
         params.append(tensor)
     return params
 
@@ -16,6 +18,7 @@ if __name__ == "__main__":
     parser.add_argument('--fused', type=str, default='False')
     parser.add_argument('--size', type=int, default=512)
     parser.add_argument('--n_params', type=int, default=10)
+    parser.add_argument('--fp16', type=str, default='False')
     args = parser.parse_args()
 
     if args.foreach == 'True':
@@ -27,10 +30,15 @@ if __name__ == "__main__":
         args.fused = True
     else:
         args.fused = False
+    
+    if args.fp16 == 'True':
+        args.fp16 = True
+    else:
+        args.fp16 = False
 
-    params = get_fake_parameters(size=args.size, n_params=args.n_params)
+    params = get_fake_parameters(size=args.size, n_params=args.n_params, fp16=args.fp16)
     optimizer = Adan(params=params, foreach=args.foreach, fused=args.fused)
+    
     torch.cuda.cudart().cudaProfilerStart()
-    for i in range(10):
-        optimizer.step()
+    optimizer.step()
     torch.cuda.cudart().cudaProfilerStop()
