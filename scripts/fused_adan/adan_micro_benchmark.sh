5e7f91d15d0c2507df16440a97d1416194259f55
Alexwell <935562189@qq.com>
2023-03-07 15:08:46 +0100

    Update adan_micro_benchmark.sh

--- scripts/fused_adan/adan_micro_benchmark.sh
+++ scripts/fused_adan/adan_micro_benchmark.sh
@@ -1,20 +1,20 @@
 clear
-echo "Adan Micro Benchmark with nvprof, size=512, n_params=10"
+echo "Adan Micro Benchmark with nvprof, size=2048, n_params=30"
 echo "===================="
 echo "foreach=False, fused=False"
 nvprof -f -o ./nvprof_log/adan_each_f_fused_f.nvvp python ./benchmark/adan_micro_benchmark.py \
-        --foreach False --fused False
+        --foreach False --fused False --size 2048 --n_params 30
 echo "===================="
 echo "foreach=True, fused=False"
 nvprof -f -o ./nvprof_log/adan_each_t_fused_f.nvvp python ./benchmark/adan_micro_benchmark.py \
-        --foreach True --fused False
+        --foreach True --fused False --size 2048 --n_params 30
 echo "===================="
 echo "foreach=False, fused=True"
 nvprof -f -o ./nvprof_log/adan_each_f_fused_t.nvvp python ./benchmark/adan_micro_benchmark.py \
-        --foreach False --fused True
+        --foreach False --fused True --size 2048 --n_params 30
 echo "===================="
 echo "foreach=True, fused=True"
 nvprof -f -o ./nvprof_log/adan_each_t_fused_t.nvvp python ./benchmark/adan_micro_benchmark.py \
-        --foreach True --fused True
+        --foreach True --fused True --size 2048 --n_params 30
 echo "===================="
 echo "Done"
