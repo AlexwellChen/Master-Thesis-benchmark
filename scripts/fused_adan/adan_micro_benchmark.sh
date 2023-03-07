7e79bc67fe3ccd84cb9a95b29b37c66d89180635
Alexwell <935562189@qq.com>
2023-03-07 15:15:26 +0100

    Update adan_micro_benchmark.sh

--- scripts/fused_adan/adan_micro_benchmark.sh
+++ scripts/fused_adan/adan_micro_benchmark.sh
@@ -2,19 +2,27 @@ clear
 echo "Adan Micro Benchmark with nvprof, size=2048, n_params=30"
 echo "===================="
 echo "foreach=False, fused=False"
-nvprof -f -o ./nvprof_log/adan_each_f_fused_f.nvvp python ./benchmark/adan_micro_benchmark.py \
+nsys profile -w true -t cuda,nvtx,osrt,cudnn,cublas -s cpu  --capture-range=cudaProfilerApi --stop-on-range-end=true --cudabacktrace=true -x true \ 
+        -o ./nvprof_log/adan_each_f_fused_f \
+        python ./benchmark/adan_micro_benchmark.py \
         --foreach False --fused False --size 2048 --n_params 30
 echo "===================="
 echo "foreach=True, fused=False"
-nvprof -f -o ./nvprof_log/adan_each_t_fused_f.nvvp python ./benchmark/adan_micro_benchmark.py \
+nsys profile -w true -t cuda,nvtx,osrt,cudnn,cublas -s cpu  --capture-range=cudaProfilerApi --stop-on-range-end=true --cudabacktrace=true -x true \ 
+        -o ./nvprof_log/adan_each_t_fused_f \
+        python ./benchmark/adan_micro_benchmark.py \
         --foreach True --fused False --size 2048 --n_params 30
 echo "===================="
 echo "foreach=False, fused=True"
-nvprof -f -o ./nvprof_log/adan_each_f_fused_t.nvvp python ./benchmark/adan_micro_benchmark.py \
+nsys profile -w true -t cuda,nvtx,osrt,cudnn,cublas -s cpu  --capture-range=cudaProfilerApi --stop-on-range-end=true --cudabacktrace=true -x true \ 
+        -o ./nvprof_log/adan_each_f_fused_t \
+        python ./benchmark/adan_micro_benchmark.py \
         --foreach False --fused True --size 2048 --n_params 30
 echo "===================="
 echo "foreach=True, fused=True"
-nvprof -f -o ./nvprof_log/adan_each_t_fused_t.nvvp python ./benchmark/adan_micro_benchmark.py \
+nsys profile -w true -t cuda,nvtx,osrt,cudnn,cublas -s cpu  --capture-range=cudaProfilerApi --stop-on-range-end=true --cudabacktrace=true -x true \ 
+        -o ./nvprof_log/adan_each_t_fused_t \
+        python ./benchmark/adan_micro_benchmark.py \
         --foreach True --fused True --size 2048 --n_params 30
 echo "===================="
 echo "Done"
