95cb10f6fc5c3972c1b07ebd508085a688e52f3c
Alexwell <935562189@qq.com>
2023-03-07 15:49:02 +0100

    Update adan_micro_benchmark.py

--- benchmark/adan_micro_benchmark.py
+++ benchmark/adan_micro_benchmark.py
@@ -12,11 +12,21 @@ def get_fake_parameters(n_params=10, size=512):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--foreach', type=bool, default=False)
-    parser.add_argument('--fused', type=bool, default=False)
+    parser.add_argument('--foreach', type=str, default='False')
+    parser.add_argument('--fused', type=str, default='False')
     parser.add_argument('--size', type=int, default=512)
     parser.add_argument('--n_params', type=int, default=10)
     args = parser.parse_args()
+
+    if args.foreach == 'True':
+        args.foreach = True
+    else:
+        args.foreach = False
+
+    if args.fused == 'True':
+        args.fused = True
+    else:
+        args.fused = False
 
     params = get_fake_parameters(size=args.size, n_params=args.n_params)
     optimizer = Adan(params=params, foreach=args.foreach, fused=args.fused)
