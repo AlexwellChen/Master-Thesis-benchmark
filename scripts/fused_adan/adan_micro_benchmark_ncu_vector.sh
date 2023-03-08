dc6a412c946f7546ec2ad3d58e4b68347f49b530
Alexwell <935562189@qq.com>
2023-03-08 10:44:33 +0100

    Update

--- scripts/fused_adan/adan_micro_benchmark_ncu_vector.sh
+++ scripts/fused_adan/adan_micro_benchmark_ncu_vector.sh
@@ -2,7 +2,7 @@ clear
 echo "Adan Micro Benchmark with ncu, size=2048, n_params=30"
 echo "===================="
 echo "foreach=False, fused=True"
-ncu -o ./ncu_log/adan_each_f_fused_t_vector --target-processes all -k "adan_cuda_kernel" --nvtx\
+ncu -o ./ncu_log/adan_each_f_fused_t_vector --target-processes all -k "adan_cuda_kernel" --nvtx --set full\
         python ./benchmark/adan_micro_benchmark.py \
         --foreach False --fused True --size 2048 --n_params 30
 echo "===================="
