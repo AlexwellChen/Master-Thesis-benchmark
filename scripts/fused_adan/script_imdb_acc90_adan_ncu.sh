a643542642f0e7974f58e7588c6618f36233a2a5
Alexwell <935562189@qq.com>
2023-03-08 18:55:22 +0100

    Update

--- scripts/fused_adan/script_imdb_acc90_adan_ncu.sh
+++ scripts/fused_adan/script_imdb_acc90_adan_ncu.sh
@@ -1,17 +1,17 @@
 clear
 echo "-----------------------Benchmark start------------------------"
-ncu -o ./ncu_log/imdb_acc90_adan_fp32 --target-processes all -k "adan_cuda_kernel" --nvtx --set full\
+ncu -o ./ncu_log/imdb_acc90_adan_fp32 --target-processes all -k "adan_cuda_kernel" --nvtx --set full -f \
         accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc.yaml \
          ./benchmark/imdb_bert_base_accelerate.py \
         --n_epochs 2 --warmup 50 \
         --lr 1e-4 --wd 0.01 \
         --optimizer adan \
-        --log_file_name imdb_adan_fused_lr1e-4_wd1e-2_wm50_ep2_acc90_mix \
+        --log_file_name imdb_adan_fused_lr1e-4_wd1e-2_wm50_ep2_acc90 \
         --target_val_acc 0.90 \
         --fused_optimizer True \
         --foreach False
 echo "--------------------------fp32 done--------------------------"
-ncu -o ./ncu_log/imdb_acc90_adan_fp16 --target-processes all -k "adan_cuda_kernel" --nvtx --set full\
+ncu -o ./ncu_log/imdb_acc90_adan_fp16 --target-processes all -k "adan_cuda_kernel" --nvtx --set full -f \
         accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
          ./benchmark/imdb_bert_base_accelerate.py \
         --n_epochs 2 --warmup 50 \
