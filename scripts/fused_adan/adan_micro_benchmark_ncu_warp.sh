12fa0e10bdda7557b5370573c21cbc2c617056d0
Alexwell <935562189@qq.com>
2023-03-08 09:46:20 +0100

    Update adan_micro_benchmark_ncu_warp.sh

--- scripts/fused_adan/adan_micro_benchmark_ncu_warp.sh
+++ scripts/fused_adan/adan_micro_benchmark_ncu_warp.sh
@@ -2,7 +2,7 @@ clear
 echo "Adan Micro Benchmark with ncu, size=2048, n_params=30"
 echo "===================="
 echo "foreach=False, fused=True"
-ncu -o ./ncu_log/adan_each_f_fused_t_warp --target-processes all -k "adan_cuda_kernel" \
+ncu -o ./ncu_log/adan_each_f_fused_t_warp --target-processes all -k "adan_cuda_kernel" --nvtx\
         python ./benchmark/adan_micro_benchmark.py \
         --foreach False --fused True --size 2048 --n_params 30
 echo "===================="
