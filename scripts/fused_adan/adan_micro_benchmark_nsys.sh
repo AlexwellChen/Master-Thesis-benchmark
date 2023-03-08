d164fc0daea50a07029f9efb3bf7eaff7a955940
Alexwell <935562189@qq.com>
2023-03-08 08:50:21 +0100

    Update scripts

--- scripts/fused_adan/adan_micro_benchmark_nsys.sh
+++ scripts/fused_adan/adan_micro_benchmark_nsys.sh
@@ -25,23 +25,4 @@ nsys profile -w true -t cuda,nvtx,osrt,cudnn,cublas -s cpu  --capture-range=cuda
         python ./benchmark/adan_micro_benchmark.py \
         --foreach True --fused True --size 2048 --n_params 30
 echo "===================="
-
-
-echo "Adan Micro Benchmark with ncu, size=2048, n_params=30"
-echo "===================="
-echo "foreach=False, fused=True"
-ncu -o ./ncu_log/adan_each_f_fused_t --target-processes all -k "adan_cuda_kernel" \
-        python ./benchmark/adan_micro_benchmark.py \
-        --foreach False --fused True --size 2048 --n_params 30
-echo "===================="
-echo "foreach=True, fused=True"
-ncu -o ./ncu_log/adan_each_t_fused_t --target-processes all -k "multi_tensor_apply_kernel"\
-        python ./benchmark/adan_micro_benchmark.py \
-        --foreach True --fused True --size 2048 --n_params 30
-echo "===================="
-echo "foreach=False, fused=True, fp16=True"
-ncu -o ./ncu_log/adan_each_f_fused_t_fp16 --target-processes all -k "adan_cuda_kernel" \
-        python ./benchmark/adan_micro_benchmark.py \
-        --foreach False --fused True --size 2048 --n_params 30 --fp16 True
-echo "===================="
 echo "Done"
