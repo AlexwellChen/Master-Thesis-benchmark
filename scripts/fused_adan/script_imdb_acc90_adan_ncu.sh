309428170e8321d1f0158f564ef5e6aa9724550f
Alexwell <935562189@qq.com>
2023-03-10 14:34:05 +0100

    Update script_imdb_acc90_adan_ncu.sh

--- scripts/fused_adan/script_imdb_acc90_adan_ncu.sh
+++ scripts/fused_adan/script_imdb_acc90_adan_ncu.sh
@@ -1,34 +1,31 @@
 clear
 pip install git+https://github.com/AlexwellChen/Adan.git
 echo "-----------------------Benchmark start------------------------"
-ncu -o ./ncu_log/imdb_acc90_adan_vector --target-processes all -k "adan_cuda_kernel" --nvtx --set full --replay-mode application \
+ncu -o ./ncu_log/imdb_acc90_adan_vector --target-processes all -k "adan_cuda_kernel" --nvtx --set full -f \
         python ./benchmark/imdb_bert_base_nvtx.py \
         --n_epochs 2 --warmup 50 \
         --lr 1e-4 --wd 0.01 \
         --optimizer adan \
         --log_file_name imdb_adan_lr1e-4_wd1e-2_wm50_ep2_acc90_nvtx \
-        --target_val_acc 0.90 \
-        --foreach False
+        --target_val_acc 0.90 --foreach False --batch_size 8 --fused_optimizer True
 echo "--------------------------vector done--------------------------"
-ncu -o ./ncu_log/imdb_acc90_adan_vector --target-processes all -k "adan_cuda_kernel" --nvtx --set full --replay-mode application \
+ncu -o ./ncu_log/imdb_acc90_adan_ILP --target-processes all -k regex:multi --nvtx --set full -f \
         python ./benchmark/imdb_bert_base_nvtx.py \
         --n_epochs 2 --warmup 50 \
         --lr 1e-4 --wd 0.01 \
         --optimizer adan \
         --log_file_name imdb_adan_lr1e-4_wd1e-2_wm50_ep2_acc90_nvtx \
-        --target_val_acc 0.90 \
-        --foreach True
+        --target_val_acc 0.90 --foreach True --batch_size 8 --fused_optimizer True
 echo "--------------------------ILP done--------------------------"
 pip uninstall adan
 pip install git+https://github.com/AlexwellChen/Adan.git@warp
-ncu -o ./ncu_log/imdb_acc90_adan_warp --target-processes all -k "adan_cuda_kernel" --nvtx --set full -f --replay-mode application \
+ncu -o ./ncu_log/imdb_acc90_adan_warp --target-processes all -k "adan_cuda_kernel" --nvtx --set full -f \
         python ./benchmark/imdb_bert_base_nvtx.py \
         --n_epochs 2 --warmup 50 \
         --lr 1e-4 --wd 0.01 \
         --optimizer adan \
         --log_file_name imdb_adan_lr1e-4_wd1e-2_wm50_ep2_acc90_ncu \
-        --target_val_acc 0.90 \ 
-        --foreach False
+        --target_val_acc 0.90 --foreach False --batch_size 8 --fused_optimizer True
 echo "----------------------------warp done---------------------------"
 # Plot the results
 # python ./benchmark/plot_loss_accuracy.py IMDB_acc92
