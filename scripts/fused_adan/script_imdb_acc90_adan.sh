6d71a9fa86d28d4bd4f50aeb46b93e259ad31c8d
Alexwell <935562189@qq.com>
2023-03-10 10:42:07 +0100

    Update script_imdb_acc90_adan.sh

--- scripts/fused_adan/script_imdb_acc90_adan.sh
+++ scripts/fused_adan/script_imdb_acc90_adan.sh
@@ -1,25 +1,25 @@
 clear
 echo "-----------------------Benchmark start------------------------"
+# python ./benchmark/imdb_bert_base.py \
+#         --n_epochs 2 --warmup 50 \
+#         --lr 1e-4 --wd 0.01 \
+#         --optimizer adan \
+#         --log_file_name imdb_adan_lr1e-4_wd1e-2_wm50_ep2_acc90 \
+#         --target_val_acc 0.90
+# echo "--------------------------adan done--------------------------"
+# python ./benchmark/imdb_bert_base.py \
+#         --n_epochs 2 --warmup 50 \
+#         --lr 1e-4 --wd 0.01 \
+#         --optimizer adan \
+#         --log_file_name imdb_adan_fused_lr1e-4_wd1e-2_wm50_ep2_acc90 \
+#         --target_val_acc 0.90 \
+#         --fused_optimizer True
+# echo "-----------------------fused adan done------------------------"
 python ./benchmark/imdb_bert_base.py \
         --n_epochs 2 --warmup 50 \
         --lr 1e-4 --wd 0.01 \
         --optimizer adan \
-        --log_file_name imdb_adan_lr1e-4_wd1e-2_wm50_ep2_acc90 \
-        --target_val_acc 0.90
-echo "--------------------------adan done--------------------------"
-python ./benchmark/imdb_bert_base.py \
-        --n_epochs 2 --warmup 50 \
-        --lr 1e-4 --wd 0.01 \
-        --optimizer adan \
-        --log_file_name imdb_adan_fused_lr1e-4_wd1e-2_wm50_ep2_acc90 \
-        --target_val_acc 0.90 \
-        --fused_optimizer True
-echo "-----------------------fused adan done------------------------"
-python ./benchmark/imdb_bert_base.py \
-        --n_epochs 2 --warmup 50 \
-        --lr 1e-4 --wd 0.01 \
-        --optimizer adan \
-        --log_file_name imdb_adan_lr1e-4_wd1e-2_wm50_ep2_acc90 \
+        --log_file_name imdb_adan_single_lr1e-4_wd1e-2_wm50_ep2_acc90 \
         --target_val_acc 0.90 \
         --foreach False
 echo "------------------------single adan done--------------------------"
@@ -27,13 +27,13 @@ python ./benchmark/imdb_bert_base.py \
         --n_epochs 2 --warmup 50 \
         --lr 1e-4 --wd 0.01 \
         --optimizer adan \
-        --log_file_name imdb_adan_fused_lr1e-4_wd1e-2_wm50_ep2_acc90 \
+        --log_file_name imdb_adan_single_fused_lr1e-4_wd1e-2_wm50_ep2_acc90 \
         --target_val_acc 0.90 \
         --fused_optimizer True \
         --foreach False
 echo "-----------------------fused single adan done------------------------"
 # Plot the results
-python ./benchmark/plot_loss_accuracy.py IMDB_acc92
+# python ./benchmark/plot_loss_accuracy.py IMDB_acc90
 # adamw:
 #     lr=1e-5, wd=0.01, warmup=160
 #     betas=(0.9, 0.999), eps=1e-8
