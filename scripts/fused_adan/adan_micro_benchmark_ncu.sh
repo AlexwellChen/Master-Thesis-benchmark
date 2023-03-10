71529db7f99e936ecbfadad499d34eaed866ed46
Alexwell <935562189@qq.com>
2023-03-10 10:49:32 +0100

    Update scripts

--- scripts/fused_adan/adan_micro_benchmark_ncu.sh
+++ scripts/fused_adan/adan_micro_benchmark_ncu.sh
@@ -1,29 +1,29 @@
 clear
-echo "Adan Micro Benchmark with ncu, size=4096, n_params=30, vector single"
+echo "Adan Micro Benchmark with ncu, size=4096, n_params=20, vector single"
 echo "===================="
 echo "foreach=False, fused=True"
 ncu -o ./ncu_log/adan_each_f_fused_t_vector --target-processes all -k "adan_cuda_kernel" --nvtx --set full\
         python ./benchmark/adan_micro_benchmark.py \
-        --foreach False --fused True --size 4096 --n_params 30
+        --foreach False --fused True --size 4096 --n_params 20
 echo "===================="
-echo "Adan Micro Benchmark with ncu, size=4096, n_params=30, ILP multi"
+echo "Adan Micro Benchmark with ncu, size=4096, n_params=20, ILP multi"
 echo "===================="
 echo "foreach=True, fused=True"
 ncu -o ./ncu_log/adan_each_t_fused_t_multi --target-processes all -k regex:multi --nvtx --set full\
         python ./benchmark/adan_micro_benchmark.py \
-        --foreach True --fused True --size 4096 --n_params 30
+        --foreach True --fused True --size 4096 --n_params 20
 echo "===================="
 echo "Uninstall vector Adan"
 pip uninstall adan
 echo "Install warp Adan"
 pip install git+https://github.com/AlexwellChen/Adan.git
 echo "===================="
-echo "Adan Micro Benchmark with ncu, size=4096, n_params=30, warp"
+echo "Adan Micro Benchmark with ncu, size=4096, n_params=20, warp"
 echo "===================="
 echo "foreach=False, fused=True"
 ncu -o ./ncu_log/adan_each_f_fused_t_warp --target-processes all -k "adan_cuda_kernel" --nvtx --set full \
         python ./benchmark/adan_micro_benchmark.py \
-        --foreach False --fused True --size 4096 --n_params 30
+        --foreach False --fused True --size 4096 --n_params 20
 echo "===================="
 echo "Uninstall warp Adan"
 pip uninstall adan
