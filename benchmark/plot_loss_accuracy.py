2f306f4aa9fadd60d0a3c5a1b22d611d40c795e9
Alexwell <935562189@qq.com>
2023-03-12 11:39:34 +0100

    Update results

--- benchmark/plot_loss_accuracy.py
+++ benchmark/plot_loss_accuracy.py
@@ -7,7 +7,7 @@ import sys
 
 def get_loss_val():
     loss_dict = {}
-    for file_path in glob.glob("./benchmark/loss_val/imdb_acc90/*"):
+    for file_path in glob.glob("./benchmark/loss_val/mix_precision/*"):
         file_name = os.path.basename(file_path)
         # Skip markdown files
         if file_name.endswith(".md"):
@@ -18,14 +18,14 @@ def get_loss_val():
         with open(file_path, "r") as f:
             lines = f.readlines()
         loss_values = [float(line.strip()) for line in lines]
-        loss_values = [sum(loss_values[max(0, i-10):i+1])/len(loss_values[max(0, i-10):i+1]) for i in range(len(loss_values))]
+        loss_values = [sum(loss_values[max(0, i-20):i+1])/len(loss_values[max(0, i-20):i+1]) for i in range(len(loss_values))]
         loss_dict[file_name] = loss_values
     loss_df = pd.DataFrame.from_dict(loss_dict, orient="index").T
     return loss_df
 
 def get_acc_val():
     acc_dict = {}
-    for file_path in glob.glob("./benchmark/acc_val/imdb_acc90/*"):
+    for file_path in glob.glob("./benchmark/acc_val/mix_precision/*"):
         file_name = os.path.basename(file_path)
         # Skip markdown files
         if file_name.endswith(".md"):
--- scripts/fused_adan/script_imdb_acc93.sh
+++ scripts/fused_adan/script_imdb_acc93.sh
@@ -4,33 +4,33 @@ python ./benchmark/imdb_bert_base.py \
         --n_epochs 2 --warmup 160 \
         --lr 1e-5 --wd 0.01 \
         --optimizer adamw \
-        --log_file_name imdb_adamw_lr1e-5_wd1e-2_wm160_ep2_acc90 \
-        --target_val_acc 0.90
+        --log_file_name imdb_adamw_lr1e-5_wd1e-2_wm160_ep2_acc93 \
+        --target_val_acc 0.93
 echo "--------------------------adamw done--------------------------"
 python ./benchmark/imdb_bert_base.py \
         --n_epochs 2 --warmup 160 \
         --lr 1e-5 --wd 0.01 \
         --optimizer adam \
-        --log_file_name imdb_adam_lr1e-5_wd1e-2_wm160_ep2_acc90 \
-        --target_val_acc 0.90
+        --log_file_name imdb_adam_lr1e-5_wd1e-2_wm160_ep2_acc93 \
+        --target_val_acc 0.93
 echo "--------------------------adam done--------------------------"
 python ./benchmark/imdb_bert_base.py \
         --n_epochs 2 --warmup 50 \
         --lr 1e-4 --wd 0.01 \
         --optimizer adan \
-        --log_file_name imdb_adan_lr1e-4_wd1e-2_wm50_ep2_acc90 \
-        --target_val_acc 0.90
+        --log_file_name imdb_adan_lr1e-4_wd1e-2_wm50_ep2_acc93 \
+        --target_val_acc 0.93
 echo "--------------------------adan done--------------------------"
 python ./benchmark/imdb_bert_base.py \
         --n_epochs 2 --warmup 50 \
         --lr 1e-4 --wd 0.01 \
         --optimizer adan \
-        --log_file_name imdb_adan_fused_lr1e-4_wd1e-2_wm50_ep2_acc90 \
-        --target_val_acc 0.90 \
+        --log_file_name imdb_adan_fused_lr1e-4_wd1e-2_wm50_ep2_acc93 \
+        --target_val_acc 0.93 \
         --fused_optimizer True
 echo "-----------------------fused adan done------------------------"
 # Plot the results
-python ./benchmark/plot_loss_accuracy.py IMDB_acc92
+# python ./benchmark/plot_loss_accuracy.py IMDB_acc92
 
 # adamw:
 #     lr=1e-5, wd=0.01, warmup=160
