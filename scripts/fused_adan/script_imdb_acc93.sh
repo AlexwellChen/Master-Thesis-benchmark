4f15749836a9ad763b1613e8346d9ad378b3ea8b
Alexwell <935562189@qq.com>
2023-03-13 09:11:18 +0100

    Update script_imdb_acc93.sh

--- scripts/fused_adan/script_imdb_acc93.sh
+++ scripts/fused_adan/script_imdb_acc93.sh
@@ -9,8 +9,26 @@ python ./benchmark/imdb_bert_base.py \
         --seed 57
 echo "--------------------------adam done--------------------------"
 python ./benchmark/imdb_bert_base.py \
+        --n_epochs 2 --warmup 160 \
+        --lr 1e-5 --wd 0.01 \
+        --optimizer adamw \
+        --log_file_name imdb_adam_lr1e-5_wd1e-2_wm160_ep2_acc93 \
+        --target_val_acc 0.93 \
+        --seed 57
+echo "--------------------------adam done--------------------------"
+accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
+         ./benchmark/imdb_bert_base_accelerate.py \
         --n_epochs 2 --warmup 50 \
-        --lr 1e-4 --wd 0.01 \
+        --lr 1e-4 --wd 0.0 \
+        --optimizer adan \
+        --log_file_name imdb_adan_fused_lr1e-4_wd1e-2_wm50_ep2_acc93_mix \
+        --target_val_acc 0.93 \
+        --fused_optimizer True \
+        --seed 57
+echo "-----------------------mix adan done------------------------"
+python ./benchmark/imdb_bert_base.py \
+        --n_epochs 2 --warmup 50 \
+        --lr 1e-4 --wd 0.005 \
         --optimizer adan \
         --log_file_name imdb_adan_lr1e-4_wd1e-2_wm50_ep2_acc93 \
         --target_val_acc 0.93 \
@@ -18,23 +36,14 @@ python ./benchmark/imdb_bert_base.py \
 echo "--------------------------adan done--------------------------"
 python ./benchmark/imdb_bert_base.py \
         --n_epochs 2 --warmup 50 \
-        --lr 1e-4 --wd 0.01 \
+        --lr 1e-4 --wd 0.005 \
         --optimizer adan \
         --log_file_name imdb_adan_fused_lr1e-4_wd1e-2_wm50_ep2_acc93 \
         --target_val_acc 0.93 \
         --fused_optimizer True \
         --seed 57
 echo "-----------------------fused adan done------------------------"
-accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
-         ./benchmark/imdb_bert_base_accelerate.py \
-        --n_epochs 2 --warmup 50 \
-        --lr 1e-4 --wd 0.01 \
-        --optimizer adan \
-        --log_file_name imdb_adan_fused_lr1e-4_wd1e-2_wm50_ep2_acc93_mix \
-        --target_val_acc 0.93 \
-        --fused_optimizer True \
-        --seed 57
-echo "-----------------------adan done------------------------"
+
 # Plot the results
 # python ./benchmark/plot_loss_accuracy.py IMDB_acc92
 
