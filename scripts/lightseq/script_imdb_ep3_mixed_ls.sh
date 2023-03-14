ada24409f5c2d2cd38a3bb20e946da2cddef40f8
skoldw <43001933+skoldw@users.noreply.github.com>
2023-03-14 08:58:17 +0100

    Updated script for LS
    Updated the script to be mixed precision, and no 93% acc val target but instead 3 epochs.

--- scripts/lightseq/script_imdb_ep3_mixed_ls.sh
+++ scripts/lightseq/script_imdb_ep3_mixed_ls.sh
@@ -2,24 +2,26 @@ clear
 echo "-----------------------Benchmark start------------------------"
 accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc.yaml \
          ./benchmark/imdb_bert_base_accelerate.py \
-        --n_epochs 2 --warmup 50 \
+        --n_epochs 3 --warmup 50 \
         --lr 1e-4 --wd 0.01 \
         --optimizer adan \
-        --log_file_name imdb_adan_fused_lr1e-4_wd1e-2_wm50_ep2_acc93_lightseq \
-        --target_val_acc 0.93 \
+        --log_file_name imdb_adan_fused_lr1e-4_wd1e-2_wm50_ep3_mixed_lightseq \
+        --fp16 True
         --fused_optimizer True \
         --batch_size 16 \
+        --seed 42
         --module_type 1
 echo "-----------------------lightseq done------------------------"
 accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc.yaml \
          ./benchmark/imdb_bert_base_accelerate.py \
-        --n_epochs 2 --warmup 50 \
+        --n_epochs 3 --warmup 50 \
         --lr 1e-4 --wd 0.01 \
         --optimizer adan \
-        --log_file_name imdb_adan_fused_lr1e-4_wd1e-2_wm50_ep2_acc93_huggingface \
-        --target_val_acc 0.93 \
+        --log_file_name imdb_adan_fused_lr1e-4_wd1e-2_wm50_ep3_mixed_huggingface \
+        --fp16 True
         --fused_optimizer True \
         --batch_size 16 \
+        --seed 42
         --module_type 0
 echo "-----------------------huggingface done------------------------"
 # Plot the results
