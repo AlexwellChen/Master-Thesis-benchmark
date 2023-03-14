29e4e8bfe408a3fdbac77af0c2f2a714ac514fb5
Alexwell <935562189@qq.com>
2023-03-14 07:36:29 +0100

    Update script_imdb_ep2_mix.sh

--- scripts/mix_percision/script_imdb_ep2_mix.sh
+++ scripts/mix_percision/script_imdb_ep2_mix.sh
@@ -5,18 +5,19 @@ accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml 
         --n_epochs 2 --warmup 50 \
         --lr 1e-4 --wd 0.01 \
         --optimizer adan \
-        --log_file_name imdb_adan_fused_lr1e-4_wd1e-2_wm50_ep2_acc93_mix \
-        --target_val_acc 0.93 \
-        --fused_optimizer True
+        --log_file_name imdb_adan_fused_lr1e-4_wd1e-2_wm50_ep2_mix \
+        --fused_optimizer True \
+        --seed 42
 echo "-----------------------adan done------------------------"
-# accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc.yaml \
-#          ./benchmark/imdb_bert_base_accelerate.py \
-#         --n_epochs 2 --warmup 50 \
-#         --lr 1e-4 --wd 0.01 \
-#         --optimizer adan \
-#         --log_file_name imdb_adan_fused_lr1e-4_wd1e-2_wm50_ep2_unmix \
-#         --fused_optimizer True
-# echo "-----------------------Mix percision done------------------------"
+accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc.yaml \
+         ./benchmark/imdb_bert_base_accelerate.py \
+        --n_epochs 2 --warmup 50 \
+        --lr 1e-4 --wd 0.01 \
+        --optimizer adan \
+        --log_file_name imdb_adan_fused_lr1e-4_wd1e-2_wm50_ep2_unmix \
+        --fused_optimizer True \
+        --seed 42
+echo "-----------------------Mix percision done------------------------"
 # Plot the results
 # python ./benchmark/plot_loss_accuracy.py IMDB_acc90
 
