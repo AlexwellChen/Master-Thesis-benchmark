4fe42241916a56631413d44e64e11d85e69033a2
Alexwell <935562189@qq.com>
2023-03-15 10:07:07 +0100

    Update imdb_bert_base_accelerate.py

--- benchmark/imdb_bert_base_accelerate.py
+++ benchmark/imdb_bert_base_accelerate.py
@@ -45,7 +45,7 @@ def data_process(args):
     test_dataset.set_format(type='torch', columns=['input_ids', 'token_type_ids', 'attention_mask', 'labels'])
     eval_dataset.set_format(type='torch', columns=['input_ids', 'token_type_ids', 'attention_mask', 'labels'])
 
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=6)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size)
     eval_loader = torch.utils.data.DataLoader(eval_dataset, batch_size=args.batch_size)
 
