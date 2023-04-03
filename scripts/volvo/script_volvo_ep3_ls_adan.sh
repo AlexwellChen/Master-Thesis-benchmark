54216b3af60939195354372a980586481c069297
Alexwell <935562189@qq.com>
2023-04-03 12:12:08 +0200

    Update script_volvo_ep3_ls_adan.sh

--- scripts/volvo/script_volvo_ep3_ls_adan.sh
+++ scripts/volvo/script_volvo_ep3_ls_adan.sh
@@ -3,7 +3,7 @@ echo "-----------------------Benchmark start------------------------"
 accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
          ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
         --n_epochs 3 --warmup 50 \
-        --lr 1e-4 --wd 0.01 \
+        --lr 5e-4 --wd 0.01 \
         --optimizer adan \
         --log_file_name volvo_adan_fused_lr1e-4_wd1e-2_wm50_ep3_mixed_ls \
         --fused_optimizer True \
