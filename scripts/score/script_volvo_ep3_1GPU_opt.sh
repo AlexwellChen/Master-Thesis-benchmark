40d3925447717aa5be2391e33f30d1d1a0b60a97
Alexwell <935562189@qq.com>
2023-04-04 14:52:27 +0200

    Update script_volvo_ep3_1GPU_opt.sh

--- scripts/score/script_volvo_ep3_1GPU_opt.sh
+++ scripts/score/script_volvo_ep3_1GPU_opt.sh
@@ -5,7 +5,7 @@ accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml 
         --n_epochs 3 --warmup 2000 \
         --lr 1e-4 --wd 0.01 \
         --optimizer adan \
-        --log_file_name volvo_adan_fused_lr1e-4_wd1e-2_wm50_ep3_mixed_ls \
+        --log_file_name volvo_adan_fused_lr1e-4_wd1e-2_wm2000_ep3_mixed_ls \
         --fused_optimizer True \
         --batch_size 16 \
         --seed 38 \
