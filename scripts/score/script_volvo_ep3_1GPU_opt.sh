4872e2eb9f6979ebb46b13a7452b3f28c5a8bdd9
Alexwell <935562189@qq.com>
2023-04-04 15:23:20 +0200

    Update script_volvo_ep3_1GPU_opt.sh

--- scripts/score/script_volvo_ep3_1GPU_opt.sh
+++ scripts/score/script_volvo_ep3_1GPU_opt.sh
@@ -1,12 +1,23 @@
 clear
-echo "-----------------------LightSeq Adan start------------------------"
-accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
+# echo "-----------------------LightSeq Adan start------------------------"
+# accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
+#          ./benchmark/volvo_bert_base_accelerate.py \
+#         --n_epochs 3 --warmup 2000 \
+#         --lr 1e-4 --wd 0.01 \
+#         --optimizer adan \
+#         --log_file_name volvo_adan_fused_lr1e-4_wd1e-2_wm2000_ep3_mixed_ls \
+#         --fused_optimizer True \
+#         --batch_size 16 \
+#         --seed 38 \
+#         --module_type 1
+echo "-----------------------HF Adan start------------------------"
+accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc.yaml \
          ./benchmark/volvo_bert_base_accelerate.py \
         --n_epochs 3 --warmup 2000 \
         --lr 1e-4 --wd 0.01 \
         --optimizer adan \
-        --log_file_name volvo_adan_fused_lr1e-4_wd1e-2_wm2000_ep3_mixed_ls \
-        --fused_optimizer True \
+        --log_file_name volvo_adan_unfused_lr1e-4_wd1e-2_wm2000_ep3_unmix_hf \
+        --fused_optimizer False \
         --batch_size 16 \
         --seed 38 \
-        --module_type 1
+        --module_type 0
