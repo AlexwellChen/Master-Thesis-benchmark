d83d7b17cc69a634b6fdc978f3ec89d1a4c6f7d4
Alexwell <935562189@qq.com>
2023-04-04 20:44:14 +0200

    Update scripts for volvo

--- scripts/score/script_volvo_ep3_1GPU_opt.sh
+++ scripts/score/script_volvo_ep3_1GPU_opt.sh
@@ -1,23 +1,34 @@
 clear
-# echo "-----------------------LightSeq Adan start------------------------"
-# accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
-#          ./benchmark/volvo_bert_base_accelerate.py \
-#         --n_epochs 3 --warmup 2000 \
-#         --lr 1e-4 --wd 0.01 \
-#         --optimizer adan \
-#         --log_file_name volvo_adan_fused_lr1e-4_wd1e-2_wm2000_ep3_mixed_ls \
-#         --fused_optimizer True \
-#         --batch_size 16 \
-#         --seed 38 \
-#         --module_type 1
-echo "-----------------------HF Adan start------------------------"
-accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc.yaml \
+echo "-----------------------LightSeq Adan FP32 start------------------------"
+accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
          ./benchmark/volvo_bert_base_accelerate.py \
         --n_epochs 3 --warmup 2000 \
         --lr 1e-4 --wd 0.01 \
         --optimizer adan \
-        --log_file_name volvo_adan_unfused_lr1e-4_wd1e-2_wm2000_ep3_unmix_hf \
+        --log_file_name volvo_adan_fused_lr1e-4_wd1e-2_wm2000_ep3_FP16_ls \
+        --fused_optimizer True \
+        --batch_size 16 \
+        --seed 38 \
+        --module_type 1
+echo "-----------------------Huggingface adamw FP32 start------------------------"
+accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc.yaml \
+         ./benchmark/volvo_bert_base_accelerate.py \
+        --n_epochs 3 --warmup 2000 \
+        --lr 5e-5 --wd 0.01 \
+        --optimizer adamw \
+        --log_file_name volvo_adamw_unfused_lr1e-5_wd1e-2_wm2000_ep3_FP32_hf \
         --fused_optimizer False \
         --batch_size 16 \
         --seed 38 \
-        --module_type 0
+        --module_type 0 
+echo "-----------------------Huggingface adan FP16 start------------------------"
+accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
+         ./benchmark/volvo_bert_base_accelerate.py \
+        --n_epochs 3 --warmup 2000 \
+        --lr 1e-4 --wd 0.01 \
+        --optimizer adan \
+        --log_file_name volvo_adan_fused_lr1e-4_wd1e-2_wm2000_ep3_FP16_hf \
+        --fused_optimizer True \
+        --batch_size 16 \
+        --seed 38 \
+        --module_type 0 
