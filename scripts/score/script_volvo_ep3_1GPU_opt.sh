9bd13967ac400cb3c8816040b8df791c7440a441
Alexwell <935562189@qq.com>
2023-04-04 14:52:19 +0200

    Update script_volvo_ep3_1GPU_opt.sh

--- scripts/score/script_volvo_ep3_1GPU_opt.sh
+++ scripts/score/script_volvo_ep3_1GPU_opt.sh
@@ -1,11 +1,11 @@
 clear
 echo "-----------------------LightSeq Adan start------------------------"
-accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc.yaml \
+accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
          ./benchmark/volvo_bert_base_accelerate.py \
-        --n_epochs 3 --warmup 50 \
+        --n_epochs 3 --warmup 2000 \
         --lr 1e-4 --wd 0.01 \
         --optimizer adan \
-        --log_file_name volvo_adan_fused_lr1e-4_wd1e-2_wm50_ep3_unmix_ls \
+        --log_file_name volvo_adan_fused_lr1e-4_wd1e-2_wm50_ep3_mixed_ls \
         --fused_optimizer True \
         --batch_size 16 \
         --seed 38 \
