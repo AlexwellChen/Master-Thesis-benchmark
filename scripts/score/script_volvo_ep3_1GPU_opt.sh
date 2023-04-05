96030a8719e9d20405408e58e8c7ffde593ddcb8
Alexwell <935562189@qq.com>
2023-04-05 10:22:09 +0200

    Update lr and warmup for opt

--- scripts/score/script_volvo_ep3_1GPU_opt.sh
+++ scripts/score/script_volvo_ep3_1GPU_opt.sh
@@ -3,9 +3,9 @@ echo "-----------------------LightSeq Adan FP16 start------------------------"
 accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
          ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
         --n_epochs 3 --warmup 2000 \
-        --lr 1e-4 --wd 0.01 \
+        --lr 5e-5 --wd 0.01 \
         --optimizer adan \
-        --log_file_name volvo_adan_fused_lr1e-4_wd1e-2_wm500_ep3_FP16_ls \
+        --log_file_name volvo_adan_fused_lr5e-5_wd1e-2_wm2000_ep3_FP16_ls \
         --fused_optimizer True \
         --batch_size 16 \
         --seed 38 \
@@ -14,21 +14,21 @@ echo "-----------------------Huggingface adamw FP32 start-----------------------
 accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc.yaml \
          ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
         --n_epochs 3 --warmup 2000 \
-        --lr 5e-5 --wd 0.01 \
+        --lr 1e-5 --wd 0.01 \
         --optimizer adamw \
-        --log_file_name volvo_adamw_unfused_lr5e-5_wd1e-2_wm2000_ep3_FP32_hf \
+        --log_file_name volvo_adamw_unfused_lr1e-5_wd1e-2_wm2000_ep3_FP32_hf \
         --fused_optimizer False \
         --batch_size 16 \
         --seed 38 \
         --module_type 0 
-echo "-----------------------Huggingface adan FP16 start------------------------"
-accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
-         ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
-        --n_epochs 3 --warmup 2000 \
-        --lr 1e-4 --wd 0.01 \
-        --optimizer adan \
-        --log_file_name volvo_adan_fused_lr1e-4_wd1e-2_wm2000_ep3_FP16_hf \
-        --fused_optimizer True \
-        --batch_size 16 \
-        --seed 38 \
-        --module_type 0 
+# echo "-----------------------Huggingface adan FP16 start------------------------"
+# accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
+#          ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
+#         --n_epochs 3 --warmup 2000 \
+#         --lr 5e-5 --wd 0.01 \
+#         --optimizer adan \
+#         --log_file_name volvo_adan_fused_lr5e-5_wd1e-2_wm2000_ep3_FP16_hf \
+#         --fused_optimizer True \
+#         --batch_size 16 \
+#         --seed 38 \
+#         --module_type 0 
