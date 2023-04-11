5e9c8ef93d4fbf20c242ea0834f73441387af9fb
Alexwell <935562189@qq.com>
2023-04-11 10:05:37 +0200

    Update script_volvo_ep3_trf_level.sh

--- scripts/volvo/script_volvo_ep3_trf_level.sh
+++ scripts/volvo/script_volvo_ep3_trf_level.sh
@@ -2,7 +2,7 @@ clear
 
 trf_level_val=2
 
-echo "-----------------------lightseq------------------------"
+echo "-----------------------lightseq 10------------------------"
 accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
          ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
         --n_epochs 3 --warmup 50 \
@@ -14,18 +14,115 @@ accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml 
         --seed 38 \
         --module_type 1 \
         --trf_level 10
-echo "-----------------------huggingface------------------------"
+echo "-----------------------lightseq 8------------------------"
 accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
          ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
         --n_epochs 3 --warmup 50 \
         --lr 1e-4 --wd 0.01 \
         --optimizer adan \
-        --log_file_name trf_10_hf \
+        --log_file_name trf_8_ls \
+        --fused_optimizer True \
+        --batch_size 16 \
+        --seed 38 \
+        --module_type 1 \
+        --trf_level 8
+echo "-----------------------lightseq 6------------------------"
+accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
+         ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
+        --n_epochs 3 --warmup 50 \
+        --lr 1e-4 --wd 0.01 \
+        --optimizer adan \
+        --log_file_name trf_6_ls \
+        --fused_optimizer True \
+        --batch_size 16 \
+        --seed 38 \
+        --module_type 1 \
+        --trf_level 6
+echo "-----------------------lightseq 4------------------------"
+accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
+         ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
+        --n_epochs 3 --warmup 50 \
+        --lr 1e-4 --wd 0.01 \
+        --optimizer adan \
+        --log_file_name trf_4_ls \
+        --fused_optimizer True \
+        --batch_size 16 \
+        --seed 38 \
+        --module_type 1 \
+        --trf_level 4
+echo "-----------------------lightseq 2------------------------"
+accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
+         ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
+        --n_epochs 3 --warmup 50 \
+        --lr 1e-4 --wd 0.01 \
+        --optimizer adan \
+        --log_file_name trf_2_ls \
+        --fused_optimizer True \
+        --batch_size 16 \
+        --seed 38 \
+        --module_type 1 \
+        --trf_level 2
+
+echo "-----------------------huggingface 10------------------------"
+accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
+         ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
+        --n_epochs 3 --warmup 50 \
+        --lr 1e-4 --wd 0.01 \
+        --optimizer adan \
+        --log_file_name trf_10_ls \
         --fused_optimizer True \
         --batch_size 16 \
         --seed 38 \
         --module_type 0 \
         --trf_level 10
+echo "-----------------------huggingface 8------------------------"
+accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
+         ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
+        --n_epochs 3 --warmup 50 \
+        --lr 1e-4 --wd 0.01 \
+        --optimizer adan \
+        --log_file_name trf_8_ls \
+        --fused_optimizer True \
+        --batch_size 16 \
+        --seed 38 \
+        --module_type 0 \
+        --trf_level 8
+echo "-----------------------huggingface 6------------------------"
+accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
+         ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
+        --n_epochs 3 --warmup 50 \
+        --lr 1e-4 --wd 0.01 \
+        --optimizer adan \
+        --log_file_name trf_6_ls \
+        --fused_optimizer True \
+        --batch_size 16 \
+        --seed 38 \
+        --module_type 0 \
+        --trf_level 6
+echo "-----------------------huggingface 4------------------------"
+accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
+         ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
+        --n_epochs 3 --warmup 50 \
+        --lr 1e-4 --wd 0.01 \
+        --optimizer adan \
+        --log_file_name trf_4_ls \
+        --fused_optimizer True \
+        --batch_size 16 \
+        --seed 38 \
+        --module_type 0 \
+        --trf_level 4
+echo "-----------------------huggingface 2------------------------"
+accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
+         ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
+        --n_epochs 3 --warmup 50 \
+        --lr 1e-4 --wd 0.01 \
+        --optimizer adan \
+        --log_file_name trf_2_ls \
+        --fused_optimizer True \
+        --batch_size 16 \
+        --seed 38 \
+        --module_type 0 \
+        --trf_level 2
 
 # adamw:
 #     lr=1e-5, wd=0.01, warmup=160
