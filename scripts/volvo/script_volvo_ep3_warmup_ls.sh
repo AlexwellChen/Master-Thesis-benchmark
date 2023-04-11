5d0c6b49f0f3e2462d52a95433698b97d9598fc5
Alexwell <935562189@qq.com>
2023-04-11 12:31:16 +0200

    Update script_volvo_ep3_warmup_ls.sh

--- scripts/volvo/script_volvo_ep3_warmup_ls.sh
+++ scripts/volvo/script_volvo_ep3_warmup_ls.sh
@@ -1,60 +1,84 @@
 clear
 
-echo "-----------------------lightseq 500------------------------"
+# echo "-----------------------lightseq 500------------------------"
+# accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
+#          ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
+#         --n_epochs 3 --warmup 500 \
+#         --lr 1e-4 --wd 0.01 \
+#         --optimizer adan \
+#         --log_file_name warmup_500_ls \
+#         --fused_optimizer True \
+#         --batch_size 16 \
+#         --seed 38 \
+#         --module_type 1 \
+#         --trf_level 10
+# echo "-----------------------lightseq 1000------------------------"
+# accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
+#          ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
+#         --n_epochs 3 --warmup 1000 \
+#         --lr 1e-4 --wd 0.01 \
+#         --optimizer adan \
+#         --log_file_name warmup_1000_ls \
+#         --fused_optimizer True \
+#         --batch_size 16 \
+#         --seed 38 \
+#         --module_type 1 \
+#         --trf_level 10
+# echo "-----------------------lightseq 1500------------------------"
+# accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
+#          ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
+#         --n_epochs 3 --warmup 1500 \
+#         --lr 1e-4 --wd 0.01 \
+#         --optimizer adan \
+#         --log_file_name warmup_1500_ls \
+#         --fused_optimizer True \
+#         --batch_size 16 \
+#         --seed 38 \
+#         --module_type 1 \
+#         --trf_level 10
+# echo "-----------------------lightseq 2000------------------------"
+# accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
+#          ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
+#         --n_epochs 3 --warmup 2000 \
+#         --lr 1e-4 --wd 0.01 \
+#         --optimizer adan \
+#         --log_file_name warmup_2000_ls \
+#         --fused_optimizer True \
+#         --batch_size 16 \
+#         --seed 38 \
+#         --module_type 1 \
+#         --trf_level 10
+# echo "-----------------------lightseq 2500------------------------"
+# accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
+#          ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
+#         --n_epochs 3 --warmup 2500 \
+#         --lr 1e-4 --wd 0.01 \
+#         --optimizer adan \
+#         --log_file_name warmup_2500_ls \
+#         --fused_optimizer True \
+#         --batch_size 16 \
+#         --seed 38 \
+#         --module_type 1 \
+#         --trf_level 10
+echo "-----------------------lightseq 3000------------------------"
 accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
          ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
-        --n_epochs 3 --warmup 500 \
+        --n_epochs 3 --warmup 3000 \
         --lr 1e-4 --wd 0.01 \
         --optimizer adan \
-        --log_file_name warmup_500_ls \
+        --log_file_name warmup_3000_ls \
         --fused_optimizer True \
         --batch_size 16 \
         --seed 38 \
         --module_type 1 \
         --trf_level 10
-echo "-----------------------lightseq 1000------------------------"
+echo "-----------------------lightseq 3500------------------------"
 accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
          ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
-        --n_epochs 3 --warmup 1000 \
+        --n_epochs 3 --warmup 3500 \
         --lr 1e-4 --wd 0.01 \
         --optimizer adan \
-        --log_file_name warmup_1000_ls \
-        --fused_optimizer True \
-        --batch_size 16 \
-        --seed 38 \
-        --module_type 1 \
-        --trf_level 10
-echo "-----------------------lightseq 1500------------------------"
-accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
-         ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
-        --n_epochs 3 --warmup 1500 \
-        --lr 1e-4 --wd 0.01 \
-        --optimizer adan \
-        --log_file_name warmup_1500_ls \
-        --fused_optimizer True \
-        --batch_size 16 \
-        --seed 38 \
-        --module_type 1 \
-        --trf_level 10
-echo "-----------------------lightseq 2000------------------------"
-accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
-         ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
-        --n_epochs 3 --warmup 2000 \
-        --lr 1e-4 --wd 0.01 \
-        --optimizer adan \
-        --log_file_name warmup_2000_ls \
-        --fused_optimizer True \
-        --batch_size 16 \
-        --seed 38 \
-        --module_type 1 \
-        --trf_level 10
-echo "-----------------------lightseq 2500------------------------"
-accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
-         ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
-        --n_epochs 3 --warmup 2500 \
-        --lr 1e-4 --wd 0.01 \
-        --optimizer adan \
-        --log_file_name warmup_2500_ls \
+        --log_file_name warmup_3500_ls \
         --fused_optimizer True \
         --batch_size 16 \
         --seed 38 \
