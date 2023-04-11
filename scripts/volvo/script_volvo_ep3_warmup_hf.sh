173f57717e518651154092f8f4a64aed19c164d9
Alexwell <935562189@qq.com>
2023-04-11 12:32:40 +0200

    Update script_volvo_ep3_warmup_hf.sh

--- scripts/volvo/script_volvo_ep3_warmup_hf.sh
+++ scripts/volvo/script_volvo_ep3_warmup_hf.sh
@@ -59,6 +59,31 @@ accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml 
         --seed 38 \
         --module_type 1 \
         --trf_level 10
+echo "-----------------------huggingface 3000------------------------"
+accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
+         ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
+        --n_epochs 3 --warmup 3000 \
+        --lr 1e-4 --wd 0.01 \
+        --optimizer adan \
+        --log_file_name warmup_3000_hf \
+        --fused_optimizer True \
+        --batch_size 16 \
+        --seed 38 \
+        --module_type 1 \
+        --trf_level 10
+echo "-----------------------huggingface 3500------------------------"
+accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc_mix.yaml \
+         ./benchmark/volvo_bert_base_accelerate_epoch_val.py \
+        --n_epochs 3 --warmup 3500 \
+        --lr 1e-4 --wd 0.01 \
+        --optimizer adan \
+        --log_file_name warmup_3500_hf \
+        --fused_optimizer True \
+        --batch_size 16 \
+        --seed 38 \
+        --module_type 1 \
+        --trf_level 10
+
 # adamw:
 #     lr=1e-5, wd=0.01, warmup=160
 #     betas=(0.9, 0.999), eps=1e-8
