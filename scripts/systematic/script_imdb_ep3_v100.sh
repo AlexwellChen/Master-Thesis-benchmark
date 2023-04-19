2c9c376acef99eb25e38584591f8f6f249a2255a
Alexwell <935562189@qq.com>
2023-04-19 19:31:54 +0200

    Update script_imdb_ep3_v100.sh

--- scripts/systematic/script_imdb_ep3_v100.sh
+++ scripts/systematic/script_imdb_ep3_v100.sh
@@ -1,15 +1,10 @@
 clear
-# creat bash array
-# optimizer_setup=(adamw adan)
-# mixed_precision_setup=(fp16 fp32)
-# lightseq_setup=(lightseq huggingface)
-# batch_size_setup=(8 16 32)
+
 optimizer_setup=(adamw)
 mixed_precision_setup=(fp16)
 lightseq_setup=(lightseq)
 batch_size_setup=(32)
 
-# write a bash loop to run all the combinations
 for optimizer in ${optimizer_setup[@]}; do
         for mixed_precision in ${mixed_precision_setup[@]}; do
                 for lightseq in ${lightseq_setup[@]}; do
@@ -27,13 +22,3 @@ for optimizer in ${optimizer_setup[@]}; do
                 done
         done
 done
-
-# adamw:
-#     lr=1e-5, wd=0.01, warmup=160
-#     betas=(0.9, 0.999), eps=1e-8
-# adan:
-#     lr=5e-5, wd=0.005, warmup=320
-#     betas=(0.98, 0.92, 0.99), eps=1e-8
-# adam:
-#     lr=1e-5, wd=0.01, warmup=160
-#     betas=(0.9, 0.999), eps=1e-8
