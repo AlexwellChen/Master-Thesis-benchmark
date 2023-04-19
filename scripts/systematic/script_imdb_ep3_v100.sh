a9e2f02f5502a0f59c9a9b5a66d13171c0e24008
Alexwell <935562189@qq.com>
2023-04-19 19:35:04 +0200

    Update script_imdb_ep3_v100.sh

--- scripts/systematic/script_imdb_ep3_v100.sh
+++ scripts/systematic/script_imdb_ep3_v100.sh
@@ -9,10 +9,14 @@ mixed_precision_setup=(fp16)
 lightseq_setup=(lightseq)
 batch_size_setup=(32)
 
-for optimizer in ${optimizer_setup[@]} do
-        for mixed_precision in ${mixed_precision_setup[@]} do
-                for lightseq in ${lightseq_setup[@]} do
-                        for batch_size in ${batch_size_setup[@]} do
+for optimizer in ${optimizer_setup[@]}
+        do
+        for mixed_precision in ${mixed_precision_setup[@]}
+                do
+                for lightseq in ${lightseq_setup[@]}
+                        do
+                        for batch_size in ${batch_size_setup[@]} 
+                                do
                                 echo "optimizer: $optimizer, mixed_precision: $mixed_precision, lightseq: $lightseq, batch_size: $batch_size"
                                 accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc.yaml \
                                         ./benchmark/systematic.py \
@@ -22,7 +26,7 @@ for optimizer in ${optimizer_setup[@]} do
                                         --optimizer $optimizer \
                                         --fp16 $mixed_precision \
                                         --lightseq $lightseq \
+                                done
                         done
                 done
         done
-done
