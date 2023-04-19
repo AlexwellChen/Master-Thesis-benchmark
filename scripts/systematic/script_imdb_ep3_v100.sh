d7dc12d03a8511aa1410eee2bd5992d53a63415b
Alexwell <935562189@qq.com>
2023-04-19 20:10:32 +0200

    Update full test scripts

--- scripts/systematic/script_imdb_ep3_v100.sh
+++ scripts/systematic/script_imdb_ep3_v100.sh
@@ -1,13 +1,9 @@
 clear
 # creat bash array
-# optimizer_setup=(adamw adan)
-# mixed_precision_setup=(fp16 fp32)
-# lightseq_setup=(lightseq huggingface)
-# batch_size_setup=(8 16 32)
-optimizer_setup=(adamw)
-mixed_precision_setup=(fp16)
-lightseq_setup=(lightseq)
-batch_size_setup=(32)
+optimizer_setup=(adamw adan)
+mixed_precision_setup=(fp16 fp32)
+lightseq_setup=(lightseq huggingface)
+batch_size_setup=(8 16 32)
 
 for optimizer in ${optimizer_setup[@]}
         do
