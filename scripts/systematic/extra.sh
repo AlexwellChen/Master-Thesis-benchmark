24df978ed8fc38adcff05078082c3a0af84c5d7b
Alexwell <935562189@qq.com>
2023-04-20 07:50:53 +0200

    Update extra.sh

--- scripts/systematic/extra.sh
+++ scripts/systematic/extra.sh
@@ -1,16 +1,18 @@
-combaination_list[0]=(adamw fp16 huggingface 32 v100)
-combaination_list[1]=(adamw no lightseq 8 v100)
-combaination_list[2]=(adamw no lightseq 16 v100)
-combaination_list[3]=(adamw no lightseq 32 v100)
-combaination_list[4]=(adamw no huggingface 32 v100)
-combaination_list[5]=(adan fp16 huggingface 32 v100)
-combaination_list[6]=(adan no lightseq 8 v100)
-combaination_list[7]=(adan no lightseq 16 v100)
-combaination_list[8]=(adan no lightseq 32 v100)
-combaination_list[9]=(adan no huggingface 32 v100)
+combaination_list[0]=('adamw fp16 huggingface 32 v100')
+combaination_list[1]=('adamw no lightseq 8 v100')
+combaination_list[2]=('adamw no lightseq 16 v100')
+combaination_list[3]=('adamw no lightseq 32 v100')
+combaination_list[4]=('adamw no huggingface 32 v100')
+combaination_list[5]=('adan fp16 huggingface 32 v100')
+combaination_list[6]=('adan no lightseq 8 v100')
+combaination_list[7]=('adan no lightseq 16 v100')
+combaination_list[8]=('adan no lightseq 32 v100')
+combaination_list[9]=('adan no huggingface 32 v100')
 
 for combaination in ${combaination_list[@]}
         do
+        # split string to array
+        combaination=(${combaination})
         echo "optimizer: ${combaination[0]}, mixed_precision: ${combaination[1]}, lightseq: ${combaination[2]}, batch_size: ${combaination[3]}, device: ${combaination[4]}"
         accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc.yaml \
                 ./benchmark/systematic.py \
