cebe34b422fabb45c82f38e65b8e0f2bfad50f31
Alexwell <935562189@qq.com>
2023-04-20 07:54:51 +0200

    Update extra.sh

--- scripts/systematic/extra.sh
+++ scripts/systematic/extra.sh
@@ -1,13 +1,13 @@
-combaination_list[0]='adamw fp16 huggingface 32 v100'
-combaination_list[1]='adamw no lightseq 8 v100'
-combaination_list[2]='adamw no lightseq 16 v100'
-combaination_list[3]='adamw no lightseq 32 v100'
-combaination_list[4]='adamw no huggingface 32 v100'
-combaination_list[5]='adan fp16 huggingface 32 v100'
-combaination_list[6]='adan no lightseq 8 v100'
-combaination_list[7]='adan no lightseq 16 v100'
-combaination_list[8]='adan no lightseq 32 v100'
-combaination_list[9]='adan no huggingface 32 v100'
+combaination_list[0]= "adamw fp16 huggingface 32 v100"
+combaination_list[1]= "adamw no lightseq 8 v100"
+combaination_list[2]= "adamw no lightseq 16 v100"
+combaination_list[3]= "adamw no lightseq 32 v100"
+combaination_list[4]= "adamw no huggingface 32 v100"
+combaination_list[5]= "adan fp16 huggingface 32 v100"
+combaination_list[6]= "adan no lightseq 8 v100"
+combaination_list[7]= "adan no lightseq 16 v100"
+combaination_list[8]= "adan no lightseq 32 v100"
+combaination_list[9]= "adan no huggingface 32 v100"
 
 for combaination in ${combaination_list[@]}
         do
