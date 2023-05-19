2d4f89b6e5180daf312110c10b23ce83f4d719cf
alexwellChen <935562189@qq.com>
2023-05-19 11:46:29 +0800

    Update systematic_multiple_times.py

--- benchmark/systematic_multiple_times.py
+++ benchmark/systematic_multiple_times.py
@@ -29,6 +29,7 @@ def seed_torch(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 
 def data_process(args):
