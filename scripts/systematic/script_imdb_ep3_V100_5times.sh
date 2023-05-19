21ac30e3b344a4fe1965b6a74badcfd9bfaa707c
alexwellChen <935562189@qq.com>
2023-05-19 12:30:51 +0800

    Update script_imdb_ep3_V100_5times.sh

--- scripts/systematic/script_imdb_ep3_V100_5times.sh
+++ scripts/systematic/script_imdb_ep3_V100_5times.sh
@@ -2,7 +2,7 @@ clear
 # creat bash array
 optimizer_setup=(adan)
 mixed_precision_setup=(fp16)
-lightseq_setup=(lightseq huggingface)
+lightseq_setup=(lightseq)
 batch_size_setup=(16)
 running_times=5
 
@@ -18,7 +18,7 @@ for ((i=1; i<=running_times; i++))
                                         do
                                         echo "optimizer: $optimizer, mixed_precision: $mixed_precision, lightseq: $lightseq, batch_size: $batch_size"
                                         accelerate launch --config_file ./accelerate_config/imdb_bert_base_acc.yaml \
-                                                ./benchmark/systematic.py \
+                                                ./benchmark/systematic_multiple_times.py \
                                                 --n_epochs 3 \
                                                 --device V100 \
                                                 --batch_size $batch_size \
