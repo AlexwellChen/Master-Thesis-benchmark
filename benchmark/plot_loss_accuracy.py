841694a8972a246cbf0415a3582b51b177ee1087
alexwellChen <935562189@qq.com>
2023-06-03 13:42:50 +0800

    Update graphs

--- benchmark/plot_loss_accuracy.py
+++ benchmark/plot_loss_accuracy.py
@@ -5,9 +5,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import sys
 
-def get_loss_val():
+def get_loss_val(name):
     loss_dict = {}
-    for file_path in glob.glob("./benchmark/loss_val/fused_unfused_3ep/*"):
+    for file_path in glob.glob("./benchmark/loss_val/"+name+"/*"):
         file_name = os.path.basename(file_path)
         # Skip markdown files
         if file_name.endswith(".md"):
@@ -16,6 +16,30 @@ def get_loss_val():
 
         if os.path.isdir(file_path):
             continue
+        
+        file_name = file_name.split(".")[0]
+
+        if name == "fused_unfused_3ep":
+            file_name = file_name.split("_")
+            if "fused" in file_name:
+                file_name = "fused"
+            elif "unfused" in file_name:
+                file_name = "unfused"
+
+        if name == "lightseq":
+            file_name = file_name.split("_")
+            if "lightseq" in file_name:
+                file_name = "lightseq"
+            elif "huggingface" in file_name:
+                file_name = "huggingface"
+
+        if name == "mix_precision":
+            file_name = file_name.split("_")
+            if "mixed" in file_name:
+                file_name = "fp16"
+            elif "unmix" in file_name:
+                file_name = "fp32"
+
         with open(file_path, "r") as f:
             lines = f.readlines()
         loss_values = [float(line.strip()) for line in lines]
@@ -24,9 +48,9 @@ def get_loss_val():
     loss_df = pd.DataFrame.from_dict(loss_dict, orient="index").T
     return loss_df
 
-def get_acc_val():
+def get_acc_val(name):
     acc_dict = {}
-    for file_path in glob.glob("./benchmark/acc_val/fused_unfused_3ep/*"):
+    for file_path in glob.glob("./benchmark/acc_val/"+name+"/*"):
         file_name = os.path.basename(file_path)
         # Skip markdown files
         if file_name.endswith(".md"):
@@ -37,7 +61,30 @@ def get_acc_val():
             continue
         with open(file_path, "r") as f:
             lines = f.readlines()
-        
+
+        file_name = file_name.split(".")[0]
+
+        if name == "fused_unfused_3ep":
+            file_name = file_name.split("_")
+            if "fused" in file_name:
+                file_name = "fused"
+            elif "unfused" in file_name:
+                file_name = "unfused"
+
+        if name == "lightseq":
+            file_name = file_name.split("_")
+            if "lightseq" in file_name:
+                file_name = "lightseq"
+            elif "huggingface" in file_name:
+                file_name = "huggingface"
+            
+        if name == "mix_precision":
+            file_name = file_name.split("_")
+            if "mixed" in file_name:
+                file_name = "fp16"
+            elif "unmix" in file_name:
+                file_name = "fp32"
+
         acc_values = [float(line.strip()) for line in lines]
         acc_dict[file_name] = acc_values
     acc_df = pd.DataFrame.from_dict(acc_dict, orient="index").T
@@ -45,18 +92,18 @@ def get_acc_val():
 
 if __name__ == "__main__":
     # get file name from os.argv
-    file_name = sys.argv[1]
-    fig, axes = plt.subplots(1, 2, figsize=(20, 10))
+    file_name = "fused_unfused_3ep"
+    fig, axes = plt.subplots(1, 2, figsize=(12, 6), dpi=160)
 
-    loss_df = get_loss_val().sort_index(axis=1)
-    acc_df = get_acc_val().sort_index(axis=1)
+    loss_df = get_loss_val(file_name).sort_index(axis=1)
+    acc_df = get_acc_val(file_name).sort_index(axis=1)
 
 
     sns.lineplot(data=loss_df, ax=axes[0])
     sns.lineplot(data=acc_df, ax=axes[1])
 
-    axes[0].set_title("Loss Curve")
-    axes[1].set_title("Accuracy Curve")
+    axes[0].set_title("Loss Curve", fontsize=12)
+    axes[1].set_title("Accuracy Curve", fontsize=12)
 
     plt.savefig("./benchmark/figure/"+file_name+"_acc_loss_curve.png")
 
--- benchmark/systematic_graph.py
+++ benchmark/systematic_graph.py
@@ -20,37 +20,49 @@ batch_size = {8: 'black', 16: 'pink', 32: 'brown'}
 device_color = "tab10"
 
 # (2, 2) subplots
-figs, ax = plt.subplots(2, 2, figsize=(12, 12))
+figs, ax = plt.subplots(2, 2, figsize=(10, 10), dpi=160)
+
 
 # 1st subplot, hue is optimizer
-ax[0, 1].set(xlabel='Time (second)', ylabel='Energy (kW·h)')
+# ax[0, 1].set(xlabel='Time (second)', ylabel='Energy (kW·h)')
+ax[0, 1].set_xlabel(xlabel='Time (second)', fontsize=12)
+ax[0, 1].set_ylabel(ylabel='Energy (kW·h)', fontsize=12)
 # subplot title
-ax[0, 1].set_title('(B) Training Energy and Time')
+ax[0, 1].set_title('(B) Training Energy and Time', fontsize=12)
 # df without TPUv2 and TPUv3
 df_no_TPU = df[(df['device']!='TPUv2') & (df['device']!='TPUv3')]
 sns.scatterplot(data=df_no_TPU, x='time', y='energy', hue='device', palette=device_color, ax=ax[0, 1])
 
 # 2nd boxplot, y is accuracy, x is time, hue is mixed_precision, shape is optimizer
-ax[1, 1].set(xlabel='Time (second)', ylabel='Accuracy (%)')
-ax[1, 1].set_title('(D) Test Accuracy and Time')
+# ax[1, 1].set(xlabel='Time (second)', ylabel='Accuracy (%)')
+ax[1, 1].set_xlabel(xlabel='Time (second)', fontsize=12)
+ax[1, 1].set_ylabel(ylabel='Accuracy (%)', fontsize=12)
+ax[1, 1].set_title('(D) Test Accuracy and Time', fontsize=12)
 sns.scatterplot(data=df, x='time', y='accuracy', hue='mixed_precision', palette=mixed_precision, ax=ax[1, 1], style='optimizer')
 
 # 3nd subplot, x is cost, y is time
-ax[1, 0].set(ylabel='Cost ($)', xlabel='Time (second)')
-ax[1, 0].set_title('(C) Training Cost and Time')
+# ax[1, 0].set(ylabel='Cost ($)', xlabel='Time (second)')
+ax[1, 0].set_xlabel(xlabel='Time (second)', fontsize=12)
+ax[1, 0].set_ylabel(ylabel='Cost ($)', fontsize=12)
+ax[1, 0].set_title('(C) Training Cost and Time', fontsize=12)
 sns.scatterplot(data=df, y='cost', x='time', hue='device', palette=device_color, ax=ax[1, 0])
 
 # 4th subplot, x is cost, y is energy
-ax[0, 0].set(ylabel='Cost ($)', xlabel='Energy (kW·h)')
-ax[0, 0].set_title('(A) Training Cost and Energy')
+# ax[0, 0].set(ylabel='Cost ($)', xlabel='Energy (kW·h)')
+ax[0, 0].set_xlabel(xlabel='Energy (kW·h)', fontsize=12)
+ax[0, 0].set_ylabel(ylabel='Cost ($)', fontsize=12)
+ax[0, 0].set_title('(A) Training Cost and Energy', fontsize=12)
 sns.scatterplot(data=df[(df['device']!='TPUv2') & (df['device']!='TPUv3')], y='cost', x='energy', hue='device', palette=device_color, ax=ax[0, 0])
 
-plt.savefig('overall_results_v3.png')
+
+plt.savefig('overall_results_v3_fnt12.png')
 
 # new figure for {v100 lightseq fused adan, batch size=16, 32}, {A100 huggingface fused adan, batch size=16 32}, {V100 huggingface fused adan, batch size=16 32}
-fig, ax = plt.subplots(1, 1, figsize=(6, 6))
-ax.set(xlabel='Time (second)', ylabel='Energy (kW·h)')
-ax.set_title('Software Optimization Energy and Time')
+fig, ax = plt.subplots(1, 1, figsize=(6, 6), dpi=160)
+# ax.set(xlabel='Time (second)', ylabel='Energy (kW·h)')
+ax.set_xlabel(xlabel='Time (second)', fontsize=12)
+ax.set_ylabel(ylabel='Energy (kW·h)', fontsize=12)
+ax.set_title('Software Optimization Energy and Time', fontsize=12)
 df_1 = df[(df['device']=='V100') & (df['module']=='lightseq') & (df['optimizer']=='adan') & (df['batch_size']==16)]
 df_2 = df[(df['device']=='V100') & (df['module']=='lightseq') & (df['optimizer']=='adan') & (df['batch_size']==32)]
 df_3 = df[(df['device']=='A100') & (df['module']=='huggingface') & (df['optimizer']=='adan') & (df['batch_size']==16)]
@@ -61,55 +73,73 @@ df_new = pd.concat([df_1, df_2, df_3, df_4, df_5])
 # drop fp32
 df_new = df_new[df_new['mixed_precision']!='fp32']
 sns.scatterplot(data=df_new, x='time', y='energy', hue='device', palette=device_color, ax=ax)
-plt.savefig('Software_Optimization_v3.png')
+plt.savefig('Software_Optimization_v3_fnt12.png')
 
 # new figure for mixed precision
-fig, ax = plt.subplots(1, 1, figsize=(6, 6))
-ax.set(xlabel='Time (second)', ylabel='Energy (kW·h)')
-ax.set_title('Mixed Precision Energy and Time')
+fig, ax = plt.subplots(1, 1, figsize=(6, 6), dpi=160)
+# ax.set(xlabel='Time (second)', ylabel='Energy (kW·h)')
+# ax.set_title('Mixed Precision Energy and Time')
+ax.set_xlabel(xlabel='Time (second)', fontsize=12)
+ax.set_ylabel(ylabel='Energy (kW·h)', fontsize=12)
+ax.set_title('Mixed Precision Energy and Time', fontsize=12)
 sns.scatterplot(data=df[(df['device']!='TPUv2') & (df['device']!='TPUv3')], x='time', y='energy', hue='device', palette=device_color, ax=ax, style='mixed_precision')
-plt.savefig('Mixed_Precision_v3.png')
+plt.savefig('Mixed_Precision_v3_fnt12.png')
 
 # new figure for different hardware on their best training time and cost
-fig, ax = plt.subplots(1, 1, figsize=(6, 6))
-ax.set(xlabel='Time (second)', ylabel='Cost ($)')
-ax.set_title('Best training time and cost for each device')
+fig, ax = plt.subplots(1, 1, figsize=(6, 6), dpi=160)
+# ax.set(xlabel='Time (second)', ylabel='Cost ($)')
+# ax.set_title('Best training time and cost for each device')
+ax.set_xlabel(xlabel='Time (second)', fontsize=12)
+ax.set_ylabel(ylabel='Cost ($)', fontsize=12)
+ax.set_title('Best training time and cost for each device', fontsize=12)
 # get rows of the best time for each device
 df_best_time = df.loc[df.groupby(['device'])['time'].idxmin()]
 print(df_best_time)
 sns.scatterplot(data=df_best_time, x='time', y='cost', hue='device', palette=device_color, ax=ax)
-plt.savefig('Best_Time_Cost_v3.png')
+plt.savefig('Best_Time_Cost_v3_fnt12.png')
 
 # new figure for different optimizer, x is time, y is accuracy
-fig, ax = plt.subplots(1, 1, figsize=(6, 6))
-ax.set(xlabel='Time (second)', ylabel='Accuracy (%)')
-ax.set_title('Optimizer Accuracy and Time')
+fig, ax = plt.subplots(1, 1, figsize=(6, 6), dpi=160)
+# ax.set(xlabel='Time (second)', ylabel='Accuracy (%)')
+# ax.set_title('Optimizer Accuracy and Time')
+ax.set_xlabel(xlabel='Time (second)', fontsize=12)
+ax.set_ylabel(ylabel='Accuracy (%)', fontsize=12)
+ax.set_title('Optimizer Accuracy and Time', fontsize=12)
 sns.scatterplot(data=df, x='time', y='accuracy', hue='optimizer', palette=optimizer, ax=ax)
-plt.savefig('Optimizer_Accuracy_Time_v3.png')
+plt.savefig('Optimizer_Accuracy_Time_v3_fnt12.png')
 
 # (2, 2) subplots for accuracy on devices, on different batch size, on different mixed precision, on different module
-figs, ax = plt.subplots(2, 2, figsize=(12, 12))
+figs, ax = plt.subplots(2, 2, figsize=(10, 10), dpi=160)
 
 # 1st subplot, hue is optimizer
-ax[0, 0].set(xlabel='Device', ylabel='Accuracy (%)')
+# ax[0, 0].set(xlabel='Device', ylabel='Accuracy (%)')
+ax[0, 0].set_ylabel(ylabel='Accuracy (%)', fontsize=12)
+ax[0, 0].set_xlabel(xlabel='Device', fontsize=12)
 # subplot title
-ax[0, 0].set_title('(A) Test Accuracy on Different Devices')
+ax[0, 0].set_title('(A) Test Accuracy on Different Devices', fontsize=12)
 sns.boxplot(data=df, x='device', y='accuracy', ax=ax[0, 0])
 
 # 2nd boxplot, y is accuracy, x is batch_size, hue is mixed_precision, shape is optimizer
-ax[0, 1].set(xlabel='Batch Size', ylabel='Accuracy (%)')
-ax[0, 1].set_title('(B) Test Accuracy on Different Batch Size')
+# ax[0, 1].set(xlabel='Batch Size', ylabel='Accuracy (%)')
+# ax[0, 1].set_title('(B) Test Accuracy on Different Batch Size')
+ax[0, 1].set_ylabel(ylabel='Accuracy (%)', fontsize=12)
+ax[0, 1].set_xlabel(xlabel='Batch Size', fontsize=12)
+ax[0, 1].set_title('(B) Test Accuracy on Different Batch Size', fontsize=12)
 sns.boxplot(data=df, x='batch_size', y='accuracy', ax=ax[0, 1])
 
 # 3nd subplot, x is module, y is accuracy
-ax[1, 0].set(ylabel='Accuracy (%)', xlabel='Module')
-ax[1, 0].set_title('(C) Test Accuracy on Different Modules')
+# ax[1, 0].set(ylabel='Accuracy (%)', xlabel='Module')
+ax[1, 0].set_ylabel(ylabel='Accuracy (%)', fontsize=12)
+ax[1, 0].set_xlabel(xlabel='Module', fontsize=12)
+ax[1, 0].set_title('(C) Test Accuracy on Different Modules', fontsize=12)
 sns.boxplot(data=df, y='accuracy', x='module', ax=ax[1, 0])
 
 # 4th subplot, x is mixed_precision, y is accuracy
-ax[1, 1].set(ylabel='Accuracy (%)', xlabel='Data types')
-ax[1, 1].set_title('(D) Test Accuracy on Different Data types')
+# ax[1, 1].set(ylabel='Accuracy (%)', xlabel='Data types')
+ax[1, 1].set_ylabel(ylabel='Accuracy (%)', fontsize=12)
+ax[1, 1].set_xlabel(xlabel='Data types', fontsize=12)
+ax[1, 1].set_title('(D) Test Accuracy on Different Data types', fontsize=12)
 sns.boxplot(data=df, y='accuracy', x='mixed_precision', ax=ax[1, 1])
 
-plt.savefig('Accuracy_with_para_v3.png')
+plt.savefig('Accuracy_with_para_v3_fnt12.png')
 
